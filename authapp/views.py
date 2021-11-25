2daae46eb4a83612807687911040994234858a62
Sergey <sergey.bukov@yandex.ru>
2021-11-25 21:43:44 +0300

    check for validity and authorization user

--- authapp/views.py
+++ authapp/views.py
@@ -5,9 +5,22 @@ from django.shortcuts import render
 from authapp.forms import UserLoginForm
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
     context = {
         'title': 'Geekshop | Авторизация',
-        'form': UserLoginForm()
+        'form': form
         }
     return render(request, 'authapp/login.html', context)
 
