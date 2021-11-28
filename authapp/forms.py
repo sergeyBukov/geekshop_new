c344eab259a1aedb1f2d4b66eb6e5d9922566879
Sergey <sergey.bukov@yandex.ru>
2021-11-28 22:45:00 +0300

    class UserProfilerForm(UserChangeForm)

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 # from django.core.exceptions import ValidationError
-# from django import forms
+from django import forms
 
 from authapp.models import User
-# from authapp.validator import validate_name
+# authapp.validator import validate_name
 
 class UserLoginForm(AuthenticationForm):
 
@@ -36,3 +36,21 @@ class UserRegisterForm(UserCreationForm):
         self.fields['password2'].widget.attrs['placeholder'] = 'Повторите пароль'
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
+
+
+class UserProfilerForm(UserChangeForm):
+    image = forms.ImageField(widget=forms.FileInput(),required=False)
+    age = forms.IntegerField(widget=forms.NumberInput(), required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name','image','age')
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfilerForm, self).__init__(*args,**kwargs)
+        self.fields['username'].widget.attrs['readonly'] = True
+        self.fields['email'].widget.attrs['readonly'] = True
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+        self.fields['image'].widget.attrs['class'] = 'custom-file-input'
