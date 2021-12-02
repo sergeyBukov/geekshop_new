214267e7cff8c5aeec63aaaa96e8512842ea1bd0
Sergey <sergey.bukov@yandex.ru>
2021-12-02 22:59:03 +0300

    for the page "products" AJAX

--- static/js/basket.js
+++ static/js/basket.js
@@ -19,7 +19,7 @@ window.onload = function () {
                 url: "/baskets/add/" + t_href + "/",
                 success: function (data) {
                     $('.card_add_basket').html(data.result)
-                    alert('товар добавлен вы корзину')
+                    // alert('товар добавлен вы корзину')
                 },
             });
         event.preventDefault()
