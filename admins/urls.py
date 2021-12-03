7211a22511490fd8be1b354a1385d166e4753101
Sergey <sergey.bukov@yandex.ru>
2021-12-03 22:00:42 +0300

    app_name = 'admins'

---
+++ admins/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+# from baskets.views import basket_add, basket_remove, basket_edit
+
+app_name = 'admins'
+urlpatterns = [
+
+    # path('add/<int:id>/', basket_add, name='basket_add'),
+
+
+]
