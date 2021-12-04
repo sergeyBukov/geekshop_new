1db14d6713e82ab24eb7bd707eecadc6fd576008
Sergey <sergey.bukov@yandex.ru>
2021-12-04 23:14:26 +0300

    class UserAdminProfileForm

--- admins/forms.py
+++ admins/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from authapp.forms import UserRegisterForm
+from authapp.forms import UserRegisterForm, UserProfilerForm
 from authapp.models import User
 from mainapp.models import ProductCategory
+
 
 class UserAdminRegisterForm(UserRegisterForm):
     image = forms.ImageField(widget=forms.FileInput(), required=False)
@@ -13,6 +14,20 @@ class UserAdminRegisterForm(UserRegisterForm):
     def __init__(self, *args, **kwargs):
         super(UserAdminRegisterForm, self).__init__(*args, **kwargs)
 
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+        self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+
+class UserAdminProfileForm(UserProfilerForm):
+    email = forms.EmailField(widget=forms.EmailInput())
+    username = forms.CharField(widget=forms.TextInput())
+
+    def __init__(self, *args, **kwargs):
+        super(UserAdminProfileForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs['readonly'] = False
+        self.fields['username'].widget.attrs['readonly'] = False
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
         self.fields['image'].widget.attrs['class'] = 'custom-file-input'
