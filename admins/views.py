4e82ed44ce15dd040465e01061877c1aad7b708b
Sergey <sergey.bukov@yandex.ru>
2021-12-04 23:15:19 +0300

    def admin_users_update def admin_users_delete

--- admins/views.py
+++ admins/views.py
@@ -1,14 +1,12 @@
-
-
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
 
-
-from admins.forms import UserAdminRegisterForm
+from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
 from authapp.models import User
+
 
 def index(request):
     return render(request, 'admins/admin.html')
@@ -36,9 +34,25 @@ def admin_users_create(request):
     return render(request, 'admins/admin-users-create.html', context)
 
 
-def admin_users_update(request):
-    return render(request, 'admins/admin-users-update-delete.html')
+def admin_users_update(request, pk):
+    user_select = User.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, instance=user_select, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=user_select)
+    context = {
+        'title': 'Geekshop - Админ | Обновление',
+        'form': form,
+        'user_select': user_select}
+    return render(request, 'admins/admin-users-update-delete.html', context)
 
 
-def admin_users_delete(request):
-    return render(request, 'admins/admin-users-update-delete.html')
+def admin_users_delete(request, pk):
+    if request.method == 'POST':
+        user = User.objects.get(pk=pk)
+        user.is_active = False
+        user.save()
+    return HttpResponseRedirect(reverse('admins:admin_users'))
