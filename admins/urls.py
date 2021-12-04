7f100d65b1f993d5bc3eb8b585cf2a827de7b309
Sergey <sergey.bukov@yandex.ru>
2021-12-04 23:14:48 +0300

    -

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from admins.views import index, admin_users_create, admin_users_update, admin_users_delete, admin_users
+
 app_name = 'admins'
 urlpatterns = [
 
@@ -8,5 +9,5 @@ urlpatterns = [
     path('users/', admin_users, name='admin_users'),
     path('users-create/', admin_users_create, name='admin_users_create'),
     path('users-update/<int:pk>', admin_users_update, name='admin_users_update'),
-    path('admin-delete/<int:pk>', admin_users_delete, name='admin_users_update'),
+    path('admin-delete/<int:pk>', admin_users_delete, name='admin_users_delete'),
 ]
