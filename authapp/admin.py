64a561c7565f23440ca1f542d6dec90899049d7e
Sergey <sergey.bukov@yandex.ru>
2021-12-06 17:49:52 +0300

    @admin.register(User)

--- authapp/admin.py
+++ authapp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from authapp.models import User
 
-admin.site.register(User)
+# admin.site.register(User)
 from baskets.admin import BasketAdmin
 from baskets.models import Basket
 
