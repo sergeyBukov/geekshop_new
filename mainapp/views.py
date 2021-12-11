ea940cf70e2535013ce83bcafbdf73a3173f970f
Sergey <sergey.bukov@yandex.ru>
2021-12-11 11:59:12 +0300

    paginator

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 
 import json
@@ -18,15 +19,26 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request, id_category=None):
+def products(request, id_category=None, page=1):
     context = {
         'title': 'Geekshop | Каталог',
     }
-    if id_category:
-        context['products'] = Product.objects.filter(category_id=id_category)
-    else:
-        context['products'] = Product.objects.all()
 
+    if id_category:
+        products = Product.objects.filter(category_id=id_category)
+    else:
+        products = Product.objects.all()
+
+    paginator = Paginator(products, per_page=3)
+
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
+    context['products'] = products_paginator
     context['categories'] = ProductCategory.objects.all()
     return render(request, 'mainapp/products.html', context)
 
@@ -38,8 +50,8 @@ class ProductDetail(DetailView):
     model = Product
     template_name = 'mainapp/detail.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(ProductDetail, self).get_context_data(**kwargs)
-        product = self.get_object()
-        context['product'] = product
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super(ProductDetail, self).get_context_data(**kwargs)
+    #     product = self.get_object()
+    #     context['product'] = product
+    #     return context
