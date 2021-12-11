c6228f27c46679fa069e84d7b1b88f0d38f50f18
Sergey <sergey.bukov@yandex.ru>
2021-12-11 14:07:48 +0300

    category

--- admins/urls.py
+++ admins/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from admins.views import index, admin_users_create, admin_users_update, admin_users_delete, admin_users
+# admin_category, admin_category_create, admin_category_update, admin_category_delete
 
 app_name = 'admins'
 urlpatterns = [
@@ -10,4 +11,10 @@ urlpatterns = [
     path('users-create/', admin_users_create, name='admin_users_create'),
     path('users-update/<int:pk>', admin_users_update, name='admin_users_update'),
     path('admin-delete/<int:pk>', admin_users_delete, name='admin_users_delete'),
+
+    # path('category/', admin_category, name='admin_category'),
+    # path('category-create/', admin_category_create, name='admin_category_create'),
+    # path('category-update/<int:pk>', admin_category_update, name='admin_category_update'),
+    # path('category-delete/<int:pk>', admin_category_delete, name='admin_category_delete'),
+
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
 # Create your views here.
 from django.urls import reverse
 
-from admins.forms import UserAdminRegisterForm, UserAdminProfileForm
+from admins.forms import UserAdminRegisterForm
+# CategoryUpdateFormAdmin
 from authapp.models import User
 
 
@@ -14,6 +14,7 @@ def index(request):
     return render(request, 'admins/admin.html')
 
 
+# Users
 @user_passes_test(lambda u: u.is_superuser)
 def admin_users(request):
     context = {
@@ -62,3 +63,62 @@ def admin_users_delete(request, pk):
         user.is_active = False
         user.save()
     return HttpResponseRedirect(reverse('admins:admin_users'))
+
+
+# Category
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_category(request):
+#     context = {
+#         'category': ProductCategory.objects.all()
+#     }
+#     return render(request, 'admins/admin-category-read.html', context)
+#
+#
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_category_create(request):
+#     if request.method == 'POST':
+#         form = CategoryUpdateFormAdmin(data=request.POST)
+#
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_category'))
+#     else:
+#         form = CategoryUpdateFormAdmin()
+#
+#     context = {
+#         'title': 'Geekshop - Админ | Категории товара',
+#         'form': form
+#     }
+#
+#     return render(request, 'admins/admin-category-create.html', context)
+#
+#
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_category_update(request, pk):
+#     category_select = ProductCategory.objects.get(pk=pk)
+#
+#     if request.method == 'POST':
+#         form = CategoryUpdateFormAdmin(data=request.POST, instance=category_select)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_category'))
+#     else:
+#         form = CategoryUpdateFormAdmin(instance=category_select)
+#
+#     context = {
+#         'title': 'Geekshop - Админ | Редактирование категории',
+#         'form': form,
+#         'category_select': category_select
+#     }
+#
+#     return render(request, 'admins/admin-category-update-delete.html', context)
+#
+#
+# @user_passes_test(lambda u: u.is_superuser)
+# def admin_category_delete(request, pk):
+#     if request.method == 'POST':
+#         user = ProductCategory.objects.get(pk=pk)
+#         user.is_active = False
+#         user.save()
+#
+#     return HttpResponseRedirect(reverse('admins:admin_category'))
