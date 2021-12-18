fb9a3b61ff7c11c2e0c7f8793d3cce21d8d6a2e5
Sergey <sergey.bukov@yandex.ru>
2021-12-18 21:19:53 +0300

    save profile

--- authapp/views.py
+++ authapp/views.py
@@ -1,18 +1,16 @@
 from django.conf import settings
 from django.contrib import auth, messages
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
 from django.views.generic import FormView, UpdateView
 
-from authapp.forms import UserLoginForm, UserRegisterForm, UserProfilerForm
+from authapp.forms import UserLoginForm, UserRegisterForm, UserProfilerForm, UserProfileEditForm
 from authapp.models import User
-from baskets.models import Basket
 from mainapp.mixin import BaseClassContextMixin, UserDispatchMixin
 
 
@@ -78,6 +76,13 @@ class ProfileFormView(UpdateView, BaseClassContextMixin, UserDispatchMixin):
     # success_message = 'OK'
     title = 'GeekShop - Профиль'
 
+    def post(self, request, *args, **kwargs):
+        form = UserProfilerForm(data=request.POST, files=request.FILES, instance=request.user)
+        profile_form = UserProfileEditForm(request.POST, instance=request.user.userprofile)
+        if form.is_valid() and profile_form.is_valid():
+            form.save()
+        return redirect(self.success_url)
+
     def form_valid(self, form):
         messages.set_level(self.request, messages.SUCCESS)
         messages.success(self.request, "Вы успешно зарегистрировались")
@@ -91,6 +96,11 @@ class ProfileFormView(UpdateView, BaseClassContextMixin, UserDispatchMixin):
     #     context = super(ProfileFormView, self).get_context_data(**kwargs)
     #     context['baskets'] = Basket.objects.filter(user=self.request.user)
     #     return context
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileFormView, self).get_context_data(**kwargs)
+        context['profile'] = UserProfileEditForm(instance=self.request.user.userprofile)
+        return context
 
 
 class Logout(LogoutView):
