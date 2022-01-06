a9021a6ee22332499b9f058dbf18a9e94d816c27
Sergey <sergey.bukov@yandex.ru>
2022-01-07 00:24:16 +0300

    get_product_price

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -2,7 +2,7 @@ from django.db import transaction
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from django.forms import inlineformset_factory
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -11,6 +11,7 @@ from django.views.generic import ListView, CreateView, DetailView, DeleteView, U
 
 from baskets.models import Basket
 from mainapp.mixin import BaseClassContextMixin
+from mainapp.models import Product
 from ordersapp.forms import OrderForm, OrderItemsForm
 from ordersapp.models import Order, OrderItem
 
@@ -120,6 +121,13 @@ def order_forming_complete(request, pk):
     return HttpResponseRedirect(reverse('orders:list'))
 
 
+def get_product_price(request,pk):
+    if request.is_ajax():
+        product = Product.objects.get(pk=pk)
+        if product:
+            return JsonResponse({'price':product.price})
+        return JsonResponse({'price':0})
+
 # при сохранении (pre_save) баскета и OrderItem  сделать следующее
 # OrderItem сохраняется при перекидке из баскета в заказ
 @receiver(pre_save, sender=Basket)
@@ -138,3 +146,4 @@ def product_quantity_update_save(sender, instance, **kwargs):
 def product_quantity_update_delete(sender, instance, **kwargs):
     instance.product.quantity += instance.quantity
     instance.product.save()
+
