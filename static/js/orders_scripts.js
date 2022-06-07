8565fe68d79afe5bbffae1f6142243b8c63d979f
Sergey <sergey.bukov@yandex.ru>
2022-06-07 14:52:31 +0300

    for the basket to work in the Profile

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -1,5 +1,22 @@
 window.onload = function () {
 
+    $('.basket_list').on('click', 'input[type="number"]', function () {
+
+        let t_href = event.target;
+        console.log(t_href.name, t_href.value);
+
+        $.ajax(
+            {
+                url: "/basket/edit/" + t_href.name + "/" + t_href.value + "/",
+                success: function (data) {
+                    $('.basket_list').html(data.result)
+                }
+            }
+        )
+
+    })
+
+
     let quantity, price, orderitem_num, delta_quantity, orderitem_quantity, delta_cost;
 
     let quantity_arr = []
